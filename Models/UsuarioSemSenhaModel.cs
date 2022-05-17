f7a1740145ea8450bb687b9ae69d4ffb7965dc91
Fabio-Jose-Americo-Santana <f.americo.santana@gmail.com>
2022-05-17 08:23:38 -0300

    Add files via upload

---
+++ Models/UsuarioSemSenhaModel.cs
@@ -0,0 +1,20 @@
+﻿using ControledeContatos.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControledeContatos.Models
+{
+    public class UsuarioSemSenhaModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do usuário")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o Login do usuário")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Digite o e-mail do usuário")]
+        [EmailAddress(ErrorMessage = " O e-mail informado não é válido!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Informe o perfil do usuário")]
+        public PerfilEnum? Perfil { get; set; }
+    }
+}
