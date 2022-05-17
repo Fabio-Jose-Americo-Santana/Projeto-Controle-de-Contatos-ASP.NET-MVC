b11b1161fedb9bd367d27ae7520f7c93402ebb4b
Fabio-Jose-Americo-Santana <f.americo.santana@gmail.com>
2022-05-17 08:22:46 -0300

    Add files via upload

---
+++ Filters/PaginaParaUsuarioLogado.cs
@@ -0,0 +1,33 @@
+﻿using ControledeContatos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+
+namespace ControledeContatos.Filters
+{
+    public class PaginaParaUsuarioLogado : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string sessaoUsuario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if(string.IsNullOrEmpty(sessaoUsuario))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { " controller", "Login" }, { "Action", "Index" } });
+            }
+            else 
+            {
+                UsuarioModel usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);
+
+                if(usuario == null)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { " controller", "Login" }, { "Action", "Index" } });
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
