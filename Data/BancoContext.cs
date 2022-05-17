2ab80fe52522e9e51333eab765c6bd889128add4
Fabio-Jose-Americo-Santana <f.americo.santana@gmail.com>
2022-05-17 08:22:09 -0300

    Add files via upload

---
+++ Data/BancoContext.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControledeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControledeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : 
+            base(options)
+
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
