46ab5662fdfec06e17ad55f38bb85d11c9a58ee3
Fabio-Jose-Americo-Santana <f.americo.santana@gmail.com>
2022-05-17 08:25:47 -0300

    Add files via upload

---
+++ ViewComponents/Menu.cs
@@ -0,0 +1,24 @@
+﻿using ControledeContatos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ControledeContatos.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string sessaoUsuario = HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario)) return null;
+
+            UsuarioModel usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);  
+
+            return View(usuario);
+        }
+         
+    }
+}
