71fc9a808b7b4bee4da27728da1b599b2210e75f
Fabio-Jose-Americo-Santana <f.americo.santana@gmail.com>
2022-05-17 08:25:23 -0300

    Add files via upload

---
+++ Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using ControledeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControledeContatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        List<ContatoModel> BuscarTodos();
+        ContatoModel ListaporId(int id);
+        ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
+    }
+}
+ 
---
+++ Repositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using ControledeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControledeContatos.Repositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        UsuarioModel BuscarPorLogin(string login);
+        List<UsuarioModel> BuscarTodos();
+        UsuarioModel ListaPorId(int id);
+        UsuarioModel Adicionar(UsuarioModel usuario);
+        UsuarioModel Atualizar(UsuarioModel usuario);
+        bool Apagar(int id);
+        UsuarioModel BuscarPorID(int id);
+    }
+}
+ 
